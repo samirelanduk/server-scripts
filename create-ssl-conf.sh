57d4c3e9a49bc02dbec2331b82495031b9469b83
Sam Ireland <sam.ireland.uk@gmail.com>
2019-06-24 23:28:30 +0100

    Create SSL script

---
+++ create-ssl-conf.sh
@@ -0,0 +1,13 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'Need website name'
+    exit 0
+fi
+
+filename=./ssl-$1.conf
+
+touch $filename
+
+cat > $filename <<- EOM
+ssl_certificate /etc/letsencrypt/live/$1/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/$1/privkey.pem;
+EOM
