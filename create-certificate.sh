2dace0cf32385c4b5a3acc0f3af40a1cd62ddfd0
Sam Ireland <sam.ireland.uk@gmail.com>
2019-06-24 23:43:55 +0100

    Create certificate script

---
+++ create-certificate.sh
@@ -0,0 +1,18 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'Need website name'
+    exit 0
+fi
+
+sitename="$1"
+
+dotcount="$(awk -F'.' '{print NF-1}' <<< $1)"
+
+if [[ $dotcount -eq 1 ]] ; then
+    sitename="$sitename -d www.$sitename"
+fi
+
+sitename="-d $sitename"
+
+echo $sitename
+
+sudo letsencrypt certonly -a webroot --webroot-path=/var/www/html $sitename
